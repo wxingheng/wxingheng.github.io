23df973fbf4e28780d36602b47376a7f7f2aa397
wuxingheng <wuxh@wuxinghengdeMacBook-Pro.local>
2019-05-13 16:51:59 +0800

    add: cdn

---
+++ dev/app.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  db: {
+    appId: 'A6088183853362',
+    // appId: 'A6063806368698',
+    // appKey: '193E1039-3882-CA44-ADB7-E4D08C590A1E'
+    appKey: '65F2035D-D001-44F3-121F-39D7AD1EAADD'
+  },
+  cdn: {
+    host: 'http://cdn.aiyuwan.cn',
+    bucket: 'wxinghenggithubio',
+    ak: 'aVzGo5g6NExTHouv9i_spzq1iZi0bPmoPeo4ZGaS',
+    sk: 'vgGbyBGrahi8Jv3qXoUqFtXPryGUidl_27DiAPwD'
+  }
+}
--- dev/src/main.js
+++ dev/src/main.js
@@ -11,7 +11,7 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 import Vant from 'vant'
-import 'vant/lib/vant-css/index.css'
+import 'vant/lib/index.css'
 
 Vue.use(Vant)
 Vue.use(ElementUI)
